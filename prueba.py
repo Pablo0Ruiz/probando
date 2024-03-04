21c19af47e522e3ee38aa85a3e5175be95131a9f
Pablo Ruiz <145439653+Pablo0Ruiz@users.noreply.github.com>
2024-03-04 01:58:02 +0100

    se modifico

--- prueba.py
+++ prueba.py
@@ -5,5 +5,7 @@ for i in range(4):
 'git add pasa el archivo al destino con el nombre del archivo'
 'git commit -m "mensaje de los cambios en el archivo'
 'comentarios'
+'git add - A manda todos los archivos al destino'
+'git push manda los archivos al repositorio'
 print('hola mundo')
 print('eso es')
