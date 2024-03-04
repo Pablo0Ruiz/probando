3e8e959c206280e3fd3fca82bd10f567cf85bf76
Pablo Ruiz <145439653+Pablo0Ruiz@users.noreply.github.com>
2024-03-04 01:22:39 +0100

    agregue print

---
+++ prueba.py
@@ -0,0 +1,8 @@
+for i in range(4):
+    print(i)
+
+'git estatus muestra los archivos del area de trabajo'
+'git add pasa el archivo al destino con el nombre del archivo'
+'git commit -m "mensaje de los cambios en el archivo'
+'comentarios'
+print('hola mundo')
